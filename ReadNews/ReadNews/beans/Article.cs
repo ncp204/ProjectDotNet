97275f93a19be7ed6ef15aa6ed997834678bfc5c
Dinh Ngoc Phu <19130169@st.hcmuaf.edu.vn>
2022-06-22 20:34:49 +0700

    update mainwindow

---
+++ ReadNews/ReadNews/beans/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Source.beans
+{
+    internal class Article
+    {
+        private string title;
+        private string content;
+        private BitmapImage image;
+
+        public Article(string title, string content, BitmapImage image)
+        {
+            this.title = title;
+            this.content = content;
+            this.image = image;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Content { get => content; set => content = value; }
+        public BitmapImage Image { get => image; set => image = value; }
+    }
+}
